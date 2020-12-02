02bee1232d39f85fedfe06e8c02da30265316eb1
Jaanus <jaanus.jurimae@khk.ee>
2020-12-02 09:41:52 -0500

    Kersti

---
+++ praks3/yl2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Muutujatele väärtus ja liitmine lauseks
+# author Jaanus
+#
+
+arv=2016.
+president="Kersti Kaljulaid"
+keskosa="aastal valiti Eesti presidendiks"
+
+echo "$arv $keskosa $president"
+
+e="Eesti vabariigi president"
+nimi="Kersti Kaljulaid"
+s="on sündinud"
+aeg="30.dets 1969"
+koht="Tartus."
+
+echo "$e $nimi $s $aeg $koht"
