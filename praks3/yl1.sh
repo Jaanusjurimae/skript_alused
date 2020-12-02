465ec3b582c6dcf405f3e70b6243e9ea06025caa
Jaanus <jaanus.jurimae@khk.ee>
2020-12-02 06:22:55 -0500

    yl1.sh

---
+++ praks3/yl1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# kasutaja eesnime ja perenime küsimine ning tervitus
+# autor - Jaanus
+#
+echo -n "Sisesta oma eesnimi ja perenimi: "
+read eesnimi perenimi
+echo "Tere tulemast, $eesnimi $perenimi"
+
