203f5ebe08100a72d374d7e2a68ca1a16ed623d6
Jaanus <jaanus.jurimae@khk.ee>
2020-12-02 15:02:25 -0500

    ridade arv kahanevalt

---
+++ arvestus/yl1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Kujund
+# kujundi koostamine sisestatud rea arvust
+# autor  Jaanus
+#
+# Ridade arv kahanevalt
+echo -n "Sisesta ridade arv: "
+read arv
+for (( i = 1; i <= $arv; i++ ))
+do
+        for (( j = $arv ; j >= $i; j-- ))
+        do
+                echo -n "*"
+        done
+        echo -e
+done
+
