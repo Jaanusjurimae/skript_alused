9485893455aa23a82cd3688288b8a895b0fbd908
Jaanus <jaanus.jurimae@khk.ee>
2020-12-02 15:12:26 -0500

    menuu.sh

---
+++ arvestus/menuu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Menüü
+# menüü esitamine ja valimine
+#
+echo -e "1) liha ja kartulid"
+echo -e "2) kala ja krõpsud"
+echo -e "3) supp ja salat"
+echo -n "Tee oma valik (1, 2 või 3): "
+read arv
+case $arv in
+	"1") echo "Väga maitsev, aga jälgi oma tervist! Head isu!";;
+	"2") echo "Brititoit on maisev! Head isu!";;
+	"3") echo "Nii tervislik ja igav toit. Head isu!";;
+	*) echo "Sellist valikut meil tänases menüüs ei ole";;
+esac
